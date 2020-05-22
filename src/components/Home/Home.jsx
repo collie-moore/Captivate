b0ed4301a81faf5405a4b8e7a7a92be4d85567bd
RobertTk1 <rtopkiffeur1@gmail.com>
2020-05-22 18:38:59 -0500

    solutions section completed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Landing from "./Landing";
 import About_home from "./About.home";
+import Solutions from "./Solutions.home";
 import "./Home.scss";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <div className="Home">
       <Landing />
       <About_home />
+      <Solutions />
     </div>
   );
 }
