764a58c84a8f51313af169053db3ce9f62c0076e
RobertTk1 <rtopkiffeur1@gmail.com>
2020-05-22 12:53:35 -0500

    landing page completed

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Landing from "./Landing";
+import "./Home.scss";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <Landing />
+    </div>
+  );
+}
