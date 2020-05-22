f177b6b3ce2181a2311240243c88c35396371fed
RobertTk1 <rtopkiffeur1@gmail.com>
2020-05-22 13:29:52 -0500

    about section on home page created

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Landing from "./Landing";
+import About_home from "./About.home";
 import "./Home.scss";
 
 export default function Home() {
   return (
     <div className="Home">
       <Landing />
+      <About_home />
     </div>
   );
 }
