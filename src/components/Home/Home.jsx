15f15ebac31f8493746074a91438de66ffb509e5
RobertTk1 <rtopkiffeur1@gmail.com>
2020-05-22 21:32:42 -0500

    news section added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Landing from "./Landing";
-import About_home from "./About.home";
+import About from "./About.home";
 import Solutions from "./Solutions.home";
 import Facts from "./Facts.home";
+import News from "./News.home";
 import "./Home.scss";
 
 export default function Home() {
   return (
     <div className="Home">
       <Landing />
-      <About_home />
+      <About />
       <Solutions />
       <Facts />
+      <News />
     </div>
   );
 }
