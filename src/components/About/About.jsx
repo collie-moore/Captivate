a77d58448553a0beb83587006a6c63db1e60374e
RobertTk1 <rtopkiffeur1@gmail.com>
2020-05-28 20:28:54 -0500

    added all 3 section and fixed padding

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import "./About.scss";
+import Section_1 from "./Sec1";
+import Section_2 from "./Sec2";
+import Section_3 from "./Sec3";
 
 export default function About() {
   return (
-    <div>
-      <h1>This is about page</h1>
+    <div className="About">
+      <Section_1 />
+      <Section_2 />
     </div>
   );
 }
---
+++ src/components/About/Sec2.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Sec2() {
+  return (
+    <div className="sec2">
+      <div className="col1">
+        <div className="show">
+          <i className="fab fa-html5 icon"></i>
+          <div>
+            <a href="#designers">For Designers</a>
+            <p>
+              Lorem ipsum dolor sit amet,Ea sed illum facere aperiam sequi optio
+              consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="image-div">
+        <img
+          src="https://demo.w3layouts.com/demos_new/template_demo/29-02-2020/captivate-liberty-demo_Free/232105078/web/assets/images/mobile.png"
+          alt="mobile site"
+        />
+      </div>
+      <div>hello 2</div>
+    </div>
+  );
+}
