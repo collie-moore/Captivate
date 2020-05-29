e27b9cc802adbdf6e6b73ba21a4e3c9956fd57af
RobertTk1 <rtopkiffeur1@gmail.com>
2020-05-29 17:24:53 -0500

    Fixed imported components naming style

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./About.scss";
-import Section_1 from "./Sec1";
-import Section_2 from "./Sec2";
-import Section_3 from "./Sec3";
+import Section1 from "./Sec1";
+import Section2 from "./Sec2";
+import Section3 from "./Sec3";
 
 export default function About() {
   return (
     <div className="About">
-      <Section_1 />
-      <Section_2 />
-      <Section_3 />
+      <Section1 />
+      <Section2 />
+      <Section3 />
     </div>
   );
 }
