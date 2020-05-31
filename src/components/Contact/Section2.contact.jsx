d59265579f4d69c2137a7bb4e85fd611c1197f48
RobertTk1 <rtopkiffeur1@gmail.com>
2020-05-30 19:11:40 -0500

    react-hook-form added

---
+++ src/components/Contact/Section2.contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export default function Section2_contact() {
+  const { register, handeSubmit } = useForm();
+  return (
+    <div className="section2">
+      <h1>Hello</h1>
+    </div>
+  );
+}
