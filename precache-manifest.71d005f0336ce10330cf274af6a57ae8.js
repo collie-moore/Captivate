83ab98a7aa2fc593478550a83ea4cbd1eebde465
RobertTk1 <rtopkiffeur1@gmail.com>
2020-05-30 23:02:38 -0500

    Updates

---
+++ precache-manifest.71d005f0336ce10330cf274af6a57ae8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd5665e72be6b84e6c4a8edba342702",
+    "url": "/Captivate/index.html"
+  },
+  {
+    "revision": "64be2aabc369b02b2fa7",
+    "url": "/Captivate/static/css/main.a08387b2.chunk.css"
+  },
+  {
+    "revision": "01447e75bd12646bd2de",
+    "url": "/Captivate/static/js/2.2bcc80e4.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/Captivate/static/js/2.2bcc80e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64be2aabc369b02b2fa7",
+    "url": "/Captivate/static/js/main.cda0d169.chunk.js"
+  },
+  {
+    "revision": "d173cacb6894cba78d51",
+    "url": "/Captivate/static/js/runtime-main.ffb022a1.js"
+  },
+  {
+    "revision": "f320143d516662569151c2b2bfd43f6f",
+    "url": "/Captivate/static/media/Background.f320143d.jpg"
+  }
+]);
